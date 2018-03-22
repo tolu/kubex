5b28708df7d561878da993603332a94a22d645f8
Tobias Lundin <tobias.lundin@gmail.com>
2018-03-22 07:38:37 +0100

    set default options in interactive listings

--- kubectl.js
+++ kubectl.js
@@ -10,21 +10,46 @@ const getNamespaces = async () => {
 };
 
 /**
- * @returns {Promise<string[]>}
+ * @returns {Promise<string>}
+ */
+const getCurrentContext = () => {
+  return execFn(['config', 'current-context']);
+};
+
+/**
+ * @typedef ClusterInfo
+ * @prop {string[]} names
+ * @prop {string[]} cluster
+ * @prop {string[]} namespace
+ * @prop {string?} current
+*/
+/**
+ * @returns {Promise<ClusterInfo>}
  */
 const getContexts = () => {
   return new Promise(async resolve => {
     const stdout = await execFn(['config', 'get-contexts']);
     // Parse output to array of contexts
-    const contexts = [];
+    const contextInfo = {
+      names: [],
+      cluster: [],
+      namespace: [],
+      current: null
+    };
     stdout.split('\n').filter(empty).forEach((line, idx) => {
       // [current, name, cluster, authInfo, namespace]
       const values = line.trim().replace(/(\*?\s)+/g, ',').split(',').filter(empty);
+      // [name, cluster, authInfo, namespace]
       if (idx > 0) {
-        contexts.push(values[0]);
+        contextInfo.names.push(values[0]);
+        contextInfo.cluster.push(values[1]);
+        contextInfo.namespace.push(values[3]);
+        if (/^\*/.test(line)) { // Current ctx line starts with '*'
+          contextInfo.current = values[0];
+        }
       }
     });
-    resolve(contexts);
+    resolve(contextInfo);
   });
 };
 
@@ -46,6 +71,7 @@ function empty(str) {
 }
 
 module.exports = {
+  getCurrentContext,
   getNamespaces,
   getContexts,
   execFn
